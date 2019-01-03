e8b97a4172f6dc75900d0e6d5086709e87d8e336
Iva Gržinić <ivagrzinic@Ivas-MacBook-Pro.local>
2019-01-03 14:00:16 +0100

    add broadcast

--- server/server.js
+++ server/server.js
@@ -13,6 +13,16 @@ app.use(express.static(publicPath));
 
 io.on('connection', socket => {
     console.log('New user connected');
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createdAt: Date.now()
+    });
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        createdAt: Date.now()
+    });
 
     socket.on('createMessage', message => {
         console.log('Created Message: ', message);
@@ -21,10 +31,19 @@ io.on('connection', socket => {
             text: message.text,
             createdAt: Date.now()
         });
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: Date.now()
+        // });
     });
 
     socket.on('disconnect', () => {
         console.log('User was disconnected');
+        socket.broadcast.emit('newMessage', {
+            from: 'Admin',
+            text: 'User left chat app'
+        });
     });
 });
 
